d4027e650544a975b01b4572e883c8bcf878160c
Youhei Oonuma <oonuma617@gmail.com>
2020-06-17 20:07:34 +0900

    init commit

---
+++ src/main/java/MadterData.kt
@@ -0,0 +1,19 @@
+val members = """
+ときのそら	tokino_sora 1
+ロボ子さん	robocosan   1
+さくらみこ   sakuramiko35    1
+星街すいせい  suisei_hosimati 1
+白神フブキ	shirakamifubuki 2,4
+夏色まつり   natsuiromatsuri    2
+赤井はあと	akaihaato   2
+湊あくあ	minatoaqua  3
+大空スバル oozorasubaru  3
+犬神ミオ    ookamimio   4
+猫又おかゆ	nekomataokayu   4
+犬神ころね	inugamikorone   4
+宝鐘マリン   houshoumarine   5
+兎田ぺこら	usadapekora 5
+潤羽るしあ	uruharushia 5
+桐生ココ	kiryucoco   6
+天音かなた	amanekanatach   6
+"""
